4415557d3241e3f2f81dab3b7da54ea1b4a87a81
augValdez <augmvaldez@gmail.com>
2021-04-22 18:44:13 -0600

    got pro profile set up

--- darciOlsen.js
+++ darciOlsen.js
@@ -6,6 +6,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 const defaultBackground = 'https://raw.githubusercontent.com/augValdez/FindAGolfPro/main/defaultBackground.jpg';
 const background = { uri: defaultBackground };
 
+const examplepro =
+  {
+    name: 'Darci Olsen',
+    title: 'Head Professional - Class A PGA Professional',
+    homecourse: 'Glenmoor Golf Course',
+    contact: 'email: darciolsen@pga.com',
+    imagelink: 'https://utahpga.com/wp-content/uploads/2019/10/IMG_1064-e1570833734766-scaled.jpg',
+    rates: 'LESSON RATES\n(Adult) Single Lesson $80/hour $40/half hour\n(Adult) 3 1-hour lesson Pack $225\n(Adult) 3 30-min lesson Pack $105\n(Junior) Single Lesson $65/hour $35/half hour\n(Junior) 3 1-hour lesson Pack $180\n(Junior) 3 30-min lesson Pack $90\n*Each additional person is $10 per lesson time\n',
+    id: 1
+  }
+
 TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
 const AppButton = ({ onPress, title }) => (
   <TouchableOpacity onPress={onPress} style={styles.buttonContainer}>
@@ -22,6 +33,10 @@ function darciOlsen({ navigation }) {
                 <AppButton title="Their Course" onPress={() => navigation.navigate('Course')} />
                 <AppButton title="Map" onPress={() => navigation.navigate('Map')} />
             </View>
+            <Text style={styles.headertext}>{examplepro.name}</Text>
+            <Text style={styles.bodytext}>{examplepro.title}</Text>
+            <Image source={{uri: examplepro.imagelink}} style={styles.profileImage} />
+            <Text style={styles.ratestext}>{examplepro.rates}</Text>
         </ImageBackground>
     </View>
     );
@@ -116,20 +131,35 @@ const styles = StyleSheet.create({
       alignSelf: "flex-start",
       margin: 20
     },
+  profileImage: {
+    width: 200,
+    height: 200,
+    alignSelf: "flex-start",
+    margin: 10,
+    marginLeft: 30
+  },
+  ratestext: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "left",
+    marginLeft: 30
+  },
   bodytext: {
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
     textAlign: "center",
 //    backgroundColor: "#000000a0",
-    margin: 20
+    margin: 0
   },
   headertext: {
     color: "white",
     fontSize: 36,
     fontWeight: "bold",
     justifyContent: "flex-start",
-    margin: 20
+    margin: 5,
+    marginLeft: 20
   },
   buttonContainer: {
     elevation: 1,
